4de235d38ee82d090ae39e81b47412274adce14b
maikelsperandio <maikelsperandio@gmail.com>
2020-03-01 10:11:42 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    username: 'root',
+    password: 'datavision',
+    database: 'poc-orm-node',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+}
